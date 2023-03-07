00f2c6b2aa3a919ec966160125d32d8425918504
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-03-08 00:06:10 +0100

    remove target

--- app/target/debug/deps/rust_course-135c87d3b4b4b517.d
+++
@@ -1,5 +0,0 @@
-/Users/frank/frank/personal/rust/rust-course/target/debug/deps/rust_course-135c87d3b4b4b517.rmeta: src/main.rs
-
-/Users/frank/frank/personal/rust/rust-course/target/debug/deps/rust_course-135c87d3b4b4b517.d: src/main.rs
-
-src/main.rs:
--- app/target/debug/deps/rust_course-46f615e789d08918.d
+++
@@ -1,5 +0,0 @@
-/Users/frank/frank/personal/rust/rust-course/target/debug/deps/rust_course-46f615e789d08918.rmeta: src/main.rs
-
-/Users/frank/frank/personal/rust/rust-course/target/debug/deps/rust_course-46f615e789d08918.d: src/main.rs
-
-src/main.rs:
