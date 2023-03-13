e832c91ce8a49adc31d0a936df116fadf3f7271c
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-03-13 16:15:50 +0100

    inline functions

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,28 +1,18 @@
 #![deny(clippy::all)]
 
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
 fn main() {
-    let mut s = String::from("hello");
-
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-
-    println!("{}, {}, ", r1, r2);
-    let r3 = &mut s; // BIG PROBLEM
-    println!("{}, ", r3);
+    let say_hello = |name: &str| name.to_string();
+    // fn say_hello(name: &str) -> String {
+    //     format!("{}", name)
+    // }
+    let greet = say_hello("f");
+    println!("{}", greet);
 }
 
-/*
-- Every value in rust has an owner
- - There can only be one owner at a time
- -  Once the owner goes out of scope the value is dropped
-
- #STRING
-  is made up of three parts
-   - a pointer to the memory that holds the contents of the string
-   - a length
-   - a capacity
-
-   RULES of REFERENCES
-   - At any given time, you can have either one mutable reference or any number of immutable refrences
-   - References must alwyas be valid
-*/
+//`{:?}` (or {:#?}
