3b9a7c76e3070597edb219c50ca838d51e8a7538
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-03-21 02:14:49 +0100

    gitignore

--- app/target/debug/rust-course.d
+++ app/target/debug/rust-course.d
@@ -1,1 +1,1 @@
-/Users/frank/frank/personal/rust/app/target/debug/rust-course: /Users/frank/frank/personal/rust/app/src/main.rs
+/Users/frank/frank/personal/rust/app/target/debug/rust-course: /Users/frank/frank/personal/rust/aggregator/src/lib.rs /Users/frank/frank/personal/rust/app/src/main.rs
